2cd19a3cc73b6f607d292911eda661de1328fb0b
kushal <kushaldeveloper@gmail.com>
2020-03-22 11:20:33 -0600

    use black
    (venv) [kushal@ideapadflex-kushal mercifulpotato]$ black .
    reformatted /home/kushal/src/py/mercifulpotato/main.py
    reformatted /home/kushal/src/py/mercifulpotato/tests/test_person.py
    All done! ✨ 🍰 ✨
    2 files reformatted, 3 files left unchanged.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from model import Person
 
-person: Person = Person.Person('Albert Einstien', 'Alby', 1987)
+person: Person = Person.Person("Albert Einstien", "Alby", 1987)
 print(person.legal_name)
