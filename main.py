053a71731121a95e01d9a874c83133bc04d1c611
kushal <kushaldeveloper@gmail.com>
2020-03-22 10:09:59 -0600

    add a new data class

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("hello, world")
+from model import Person
+
+person: Person = Person.Person('Albert Einstien', 'Alby', 1987)
+print(person.legal_name)
---
+++ model/Person.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Person:
+    legal_name: str
+    preferred_name: str
+    year_of_birth: int
+
+    def __init__(self, legal_name: str, preferred_name: str, year_of_birth: int):
+        self.legal_name = legal_name
+        self.preferred_name = preferred_name
+        self.year_of_birth = year_of_birth
