fd68da45d557aba2ba91dd967c43112ac6ca38ab
kushal <kushaldeveloper@gmail.com>
2020-09-01 19:19:31 -0600

    actually fix unit test

--- tests/test_person.py
+++ tests/test_person.py
@@ -3,4 +3,4 @@ from model import Person
 
 def test_person_age():
     person: Person = Person.Person("Albert Einstein", "Alby", 1879)
-    assert person.get_age() > 100
+    assert person.get_age(2020) == 141
