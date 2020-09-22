bc79fac3271f7ce8ed539fd28dc981cac4a5acbc
kushal <kushaldeveloper@gmail.com>
2020-09-21 18:38:01 -0600

    reach out to server

--- r1sxvinfo.py
+++ r1sxvinfo.py
@@ -4,6 +4,8 @@
 
 import itertools
 
+import requests
+
 upper_case = [chr(item) for item in range(ord('A'), ord('Z') + 1)]
 lower_case = [chr(item) for item in range(ord('A'), ord('Z') + 1)]
 numbers = [chr(item) for item in range(ord('0'), ord('9') + 1)]
@@ -11,4 +13,13 @@ numbers = [chr(item) for item in range(ord('0'), ord('9') + 1)]
 my_input = upper_case + lower_case + numbers
 
 for my_input in map(''.join, itertools.product(my_input, repeat=10)):
-    print(my_input)
+    my_url = f"http://r1sxv.info/{my_input}"
+    max_retries = 20
+    session = requests.Session()
+    adapter = requests.adapters.HTTPAdapter(max_retries=max_retries)
+    session.mount("https://", adapter)
+    session.mount("http://", adapter)
+    comment_data = session.get(
+        my_url, headers={"Cache-Control": "no-cache", "Pragma": "no-cache", },
+    )
+    print(comment_data)
